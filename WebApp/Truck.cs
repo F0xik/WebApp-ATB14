9d98c67985ea06bde6623f2e07a4962e4f786f37
F0xik <tanasov0672@mail.ru>
2025-03-05 07:10:14 +0300

    DesignBlocks1-3

---
+++ WebApp/Truck.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Components
+{
+    public class Truck
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Phone { get; set; }
+
+        public string Description { get; set; }
+    }
+}
