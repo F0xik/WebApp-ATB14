7e12c7d5c6d4bfd691e39db8becc34eaefb19cd3
F0xik <tanasov0672@mail.ru>
2025-03-07 00:16:31 +0300

    Home Pade Done2

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d98c67985ea06bde6623f2e07a4962e4f786f37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2043c72879ac07794fdaecf9d62b2c14bb54844a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
